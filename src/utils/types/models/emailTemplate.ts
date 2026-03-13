e53b01216338e4f63a9e7d8137ec049426cb8484
odutradev <joao.vitornl@gmail.com>
2026-03-13 11:44:29 -0300

    fix: Fix trigger typing in email template model to support dynamic strings

--- src/utils/types/models/emailTemplate.ts
+++ src/utils/types/models/emailTemplate.ts
@@ -1,11 +1,11 @@
 export type EmailTemplateModelType = {
-    _id?: string;
-    trigger: "PASSWORD_RESET" | "PASSWORD_CHANGED";
-    subject: string;
-    markdownBody: string;
-    variables: string[];
-    active: boolean;
-    createdAt?: Date | string;
-    updatedAt?: Date | string;
-    description?: string;
+  _id?: string;
+  trigger: string;
+  subject: string;
+  markdownBody: string;
+  variables: string[];
+  active: boolean;
+  createdAt?: Date | string;
+  updatedAt?: Date | string;
+  description?: string;
 };
